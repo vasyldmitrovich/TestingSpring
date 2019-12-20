e73c5973d85009c158ea7ce6a8e94c4b0e57116f
VasylBazhan <Rapidvasyaalister@gmail.com>
2019-12-20 22:51:45 +0200

    This is first project for String

---
+++ src/main/java/org/vasyldmitrovich/firstspring/TestBean.java
@@ -0,0 +1,14 @@
+package org.vasyldmitrovich.firstspring;
+
+public class TestBean {
+    private String name;
+    public TestBean (String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+}
