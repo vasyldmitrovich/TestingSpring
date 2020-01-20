3529f0d12436dccd9823cd5fb6b668de21f878e6
VasylBazhan <Rapidvasyaalister@gmail.com>
2020-01-21 00:10:18 +0200

    Add a few field and some logic

--- src/main/java/org/vasyldmitrovich/firstspring/TestBean.java
+++ src/main/java/org/vasyldmitrovich/firstspring/TestBean.java
@@ -2,8 +2,12 @@ package org.vasyldmitrovich.firstspring;
 
 public class TestBean {
     private String name;
-    public TestBean (String name){
+    private int age;
+    private boolean gender;
+    public TestBean (String name, int age, boolean gender){
         this.name = name;
+        this.age = age;
+        this.gender = gender;
     }
     public String getName(){
         return name;
@@ -11,4 +15,17 @@ public class TestBean {
     public void setName(String name){
         this.name = name;
     }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public boolean getGender(){
+        return gender;
+    }
+    public void setGender(boolean gender){
+        this.gender = gender;
+    }
+
 }
--- src/main/java/org/vasyldmitrovich/firstspring/TestSpring.java
+++ src/main/java/org/vasyldmitrovich/firstspring/TestSpring.java
@@ -7,7 +7,17 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
         TestBean testBean = context.getBean("testBean",TestBean.class);
-        System.out.println(testBean.getName());
+        boolean menOrWomen = testBean.getGender();
+        String genderIs = "";
+        if (menOrWomen) {
+            genderIs = "Men";
+        }
+        else {
+            genderIs = "Woman";
+        }
+        System.out.println("This person name is: "+testBean.getName()+"\n" +
+                "This is: "+genderIs+"\n" +
+                "and is: "+testBean.getAge()+" years old");
         context.close();
     }
 }
